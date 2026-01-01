cf183e256cb9a4c5c06d90283b82355c68c2fa39
George Leyva <leyvageorge@hotmail.com>
2026-01-01 00:58:35 -0800

    removed file added final project

---
+++ ITS320_week8_Jorge.py
@@ -0,0 +1,83 @@
+#Student Course Registration System
+"""
+1.User Authentication:
+Implement a basic login system with predefined usernames and passwords for students and administrators. 
+Use the ID “admin” for the Administrator, with password “password”.
+2.Course Management:
+Course Management by Admin:
+- Add new courses.
+- Remove courses.
+- Update course details (like title, description, credits, capacity).- 
+- Search for courses by title or course ID.
+Student Course Registration:
+- Allow students to register for courses.
+- Allow students to drop courses.
+3.Reports:
+ADMIN:
+- List all students registered for a specific course.
+- List all courses for which a specific student registered.
+- List of all student IDs and passwords.
+STUDENTS:
+- List all courses for which this student registered.
+BOTH:
+- List all courses (showing if each is still full or not.)
+4.Additional Features:
+- Implement capacity limits for courses and prevent registration if the course is full.
+
+"""
+# ==========  CLASSES  ==========
+class Course:
+    def __init__(self,title,id,credit,capacity,description):
+        self.title 
+        self.id 
+        self.credit 
+        self.capacity
+        self.description
+
+class Person:
+    def __init__(self):
+        self.f_name
+        self.l_name
+
+class Admin:
+    def __init__(self,fname,lname):
+        Person.__init__(self,fname,lname)
+        self.admin_ID = "admin"
+        self.admin_Password = "password"
+
+class Student:
+    def __init__(self,fname,lname):
+        Person.__init__(self,fname,lname)
+        self.user_name
+        self.password
+        self.reg_course = []
+
+# ========== FUNCTION  ==========
+# ==========   SETUP   ==========
+class_list = []
+catalog = [
+    ("Intro Art","AR100",3,30,"Develop skiils to successfully draw the human form." ),
+    ("English 101","EN101",3,25,"Students receive instruction in academic reading and writing, including writing processes, effective use of language, analytical thinking, and the foundations of academic research."),
+    ("Algebra","MA105",3,30,"This course covers graphing of polynomial, rational and transcendental functions." ),
+    ("Calculus","MA135",3,25,"This course covers functions and their graphs, including exponential and logarithmic functions."),
+    ("Anatomy & Physiology","AP102",5,20,"This course is the first in a two part series covering the topics of the chemical, cellular, and tissue levels of organization"),
+    ("Beginning Guitar","MU054",1, 5,"This course offers individual guitar instruction to students who have little or no previous training.")
+]
+students = [
+    ("Liam","Smith","LiaSm95","owU9-A3WD",[]),
+    ("Olivia","Taylor","OliTa01","25le-C87z",[]),
+    ("Oliver","White","OliWh05","0glu-zi1D",[]),
+    ("Emily","Leyva","EmiLe02","vcnJ-Gl98",[]),
+    ("Levi","Clark","LevCl90","WZ7o-n5QJ",[]),
+    ("Isabella","Ferrero","IsaFe03","SDg9-DT9l",[]),
+    ("William","Young","WilYo97","J38D-NXN3",[]),
+    ("Ava","King","AvaKi99","QX1I-sswP",[]),
+    ("James","Green","JamGr06","4b9Y-beOD",[]),
+    ("Sofia","Collins","SofCo02","yxam-Iy1u",[])
+]
+# ==========   MAIN    ==========
+print("Welcome to the Registration System")
+#Ask for user login
+#Menu for Student or Admin
+#Exit Menu
+print("Have a nice day")
